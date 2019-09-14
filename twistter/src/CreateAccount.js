96f79ea00d09fb21b5a9a1198c6b3ee1cc8b12e7
Netra Pradhan <npradha@purdue.edu>
2019-09-14 00:04:13 -0400

    Add basic css to center create account

--- twistter/src/CreateAccount.js
+++ twistter/src/CreateAccount.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
+import './CreateAccount.css';
 
 class CreateAccount extends Component {
     render(){
     return (
         <div className="CreateAccount">
-            <h1> TWISTTER </h1>
-            <h3> Create Account </h3>
-            <form>
-                First Name: <br/>
-                <input type="text" name="firstname" value="" focus/><br></br>
-                Last Name: <br/>
-                <input type="text" name="lastname" value=""/><br></br>
-                Email:<br/> 
-                <input type="text" name="email" value="" /><br></br>
-                Password: <br/>
-                <input type="text" name="password" value=""/><br></br>
-                Confirm Password: <br/>
-                <input type="text" name="passwordConfirm" value=""/><br></br>
-                Handle: <br/> 
-                @<input type="text" name="passwordConfirm" value=""/><br></br>
-                <input type="submit" value="Submit"/>
-            </form>
-        
+            <div className="inputBox">
+                <h1> TWISTTER </h1>
+                <h3> Create Account </h3>
+                <form>
+                    First Name: <br/>
+                    <input type="text" name="firstname" value="" focus/><br></br>
+                    Last Name: <br/>
+                    <input type="text" name="lastname" value=""/><br></br>
+                    Email:<br/> 
+                    <input type="text" name="email" value="" /><br></br>
+                    Password: <br/>
+                    <input type="text" name="password" value=""/><br></br>
+                    Confirm Password: <br/>
+                    <input type="text" name="passwordConfirm" value=""/><br></br>
+                    @ <br/> 
+                    <input type="text" name="passwordConfirm" value=""/><br></br>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
         
         
         
