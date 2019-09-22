14eaadd32489ed34ae8cef66e9d90d95ccc16ec4
albertzhong <albert.zhong1999@gmail.com>
2019-09-22 19:19:21 -0400

    App js handles redirection to timeline page

--- twistter/src/App.js
+++ twistter/src/App.js
@@ -1,36 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Timeline from './Timeline.js'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
-
+import { Redirect } from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
-
-
-
   return (
     <div className="App">
-      <header className="App-header">
-          <Navbar>
-              <Nav.Link href="#home">Home</Nav.Link>
-          </Navbar>
-          <div className="SearchBar">
-            <Form>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </div>
-          <Navbar>
-              <Nav.Link href="#profile">Profile Page</Nav.Link>
-          </Navbar>
-      </header>
-      <body>
-        <h1> twistter </h1>
-      </body>
+      <div className="App-header">
+          <h1>Twistter</h1>
+      </div>
+        <Switch>
+            <Route path="/timeline" component={Timeline}/>
+            <Route render= {() =>
+                <Timeline />
+            }/>
+        </Switch>
 
     </div>
 
