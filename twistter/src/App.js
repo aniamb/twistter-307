b13735a7dab24b56a84eff1ced21bf7aa37817b6
Polymnia Nicolaou <polymnia@purdue.edu>
2019-09-22 20:31:32 -0400

    Update edit profile

--- twistter/src/App.js
+++ twistter/src/App.js
@@ -3,12 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import CreateAccount from './CreateAccount'
 import Login from './Login'
-<<<<<<< HEAD
 import EditProfile from './EditProfile'
-
-=======
 import Timeline from './Timeline.js'
->>>>>>> origin/master
 import {Switch, Route} from 'react-router-dom'
 
 function App() {
@@ -18,25 +14,16 @@ function App() {
       <h1>Twistter</h1>
     </div>
       <Switch>
-<<<<<<< HEAD
-        <Route path="/createaccount" component={CreateAccount}/>
-        <Route path="/login" component={Login}/>
-        <Route path="/editprofile" component={EditProfile}/>
-        <Route render= {() =>
-            <CreateAccount />
-           }/>
-    </Switch>
-=======
           <Route path="/createaccount" component={CreateAccount}/>
           <Route path="/login" component={Login}/>
-  //      <Route path="/timeline" component={Timeline}/>
-  //      <Route render= {() =>
-  //        <Timeline />
+          <Route path="/editprofile" component={EditProfile}/>
+          {/* <Route path="/timeline" component={Timeline}/>
+          <Route render= {() =>
+            <Timeline /> */}
           <Route render= {() =>
               <CreateAccount />
              }/>
       </Switch>
->>>>>>> origin/master
     </div>
 
   );
