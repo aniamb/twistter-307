5c9b85a17578bfd0b3a2c54a14df4c8b31f4548a
albertzhong <albert.zhong1999@gmail.com>
2019-09-27 00:03:21 -0400

    Removed import statements that were used before moving Timeline

--- twistter/src/App.js
+++ twistter/src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Timeline from './Timeline.js'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
+import Timeline from './Timeline'
+import Search from './Search'
 import { Redirect } from 'react-router-dom'
 import {Switch, Route} from 'react-router-dom'
 
@@ -18,6 +14,7 @@ function App() {
       </div>
         <Switch>
             <Route path="/timeline" component={Timeline}/>
+            <Route path="/search" component={Search}/>
             <Route render= {() =>
                 <Timeline />
             }/>
--- twistter/src/Timeline.js
+++ twistter/src/Timeline.js
@@ -1,11 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
+
 
 import { Redirect } from 'react-router-dom'
 
@@ -13,23 +9,6 @@ class Timeline extends Component{
     render() {
         return (
             <div className="Timeline">
-                {/*<header className="App-header">*/}
-                {/*    <Navbar>*/}
-                {/*        <Nav.Link href="#home">Home</Nav.Link>*/}
-                {/*    </Navbar>*/}
-                {/*    <div className="SearchBar">*/}
-                {/*        <Form>*/}
-                {/*            <FormControl type="text" placeholder="Search" className="mr-sm-2"/>*/}
-                {/*            <Button variant="outline-success">Search</Button>*/}
-                {/*        </Form>*/}
-                {/*    </div>*/}
-                {/*    <Navbar>*/}
-                {/*        <Nav.Link href="#profile">Profile Page</Nav.Link>*/}
-                {/*    </Navbar>*/}
-                {/*</header>*/}
-                {/*<body>*/}
-                {/*<h1> twistter </h1>*/}
-                {/*</body>*/}
 
                 <div className="links">
                     <ul className="navLinks">
