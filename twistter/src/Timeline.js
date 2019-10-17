dada2b017ecd8bed6677d9c6b7c9114db000d0ed
Anita <aambalav@purdue.edu>
2019-10-16 20:15:12 -0400

    Intial timeline setup

--- twistter/src/Timeline.js
+++ twistter/src/Timeline.js
@@ -21,21 +21,37 @@ class Timeline extends Component{
     render() {
         return (
             <div className="Timeline">
-                <div className="links">
-                    <ul className="navLinks">
-                        <li><NavLink to="/Timeline">Twistter</NavLink></li>
-                        {/* <li><NavLink exact to="/">home</NavLink></li> */}
-                        <li>My Profile</li>
-                        <li>
-                            <form action="http://localhost:5000/searchserver" method="post">
-                                {/*Redirect to search in backend*/}
-                                Search users: <br/>
-                                <input type="text" placeholder="Search.." name="searchparam"></input>
-                                <br/>
-                                <button type="submit">Click To Search<i className="fa fa-search"></i></button>
-                            </form>
-                        </li>
-                    </ul>
+
+                <div class="row">
+                  <div className="sidebar" >
+                    <div className="links">
+                        <ul className="navLinks">
+                            <li><NavLink to="/Timeline">Twistter</NavLink></li>
+                            {/* <li><NavLink exact to="/">home</NavLink></li> */}
+                            <li>My Profile</li>
+                            <li>
+                                <form action="http://localhost:5000/searchserver" method="post">
+                                    {/*Redirect to search in backend*/}
+                                    Search users: <br/>
+                                    <input type="text" placeholder="Search.." name="searchparam"></input>
+                                    <br/>
+                                    <button type="submit">Click To Search<i className="fa fa-search"></i></button>
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                  </div>
+                  <div className="microOrder">
+                    <div className="microblogs">
+                      <h1> I really like tennis. </h1>
+                    </div>
+                    <div className="microblogs">
+                      <h1> CS 307 at purdue is really trash. </h1>
+                    </div>
+                    <div className="microblogs">
+                      <h1> Boiler Up! </h1>
+                    </div>
+                  </div>
                 </div>
             </div>
 
