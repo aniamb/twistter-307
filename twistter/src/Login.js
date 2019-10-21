84bb15770844f018f3ac2e434afd9322399ff73c
Netra Pradhan <npradha@purdue.edu>
2019-10-20 21:15:48 -0400

    Add HTML alert

--- twistter/src/Login.js
+++ twistter/src/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Switch, Route, NavLink, Redirect} from 'react-router-dom'
 import axios from 'axios'
+import { Alert } from 'react-alert'
 
 import './Login.css';
 
@@ -11,7 +12,8 @@ class Login extends Component {
             email: '',
             password:'',
             isSubmitted: false,
-            isRedirect: false
+            isRedirect: null,
+            receivedRequest: false
         }
     }
 
@@ -23,6 +25,8 @@ class Login extends Component {
     }
     handleSubmit(event){ 
         event.preventDefault();
+        event.target.reset();
+        this.setState({receivedRequest: true});
         const loginInfo = {email: this.state.email, password:this.state.password};
         axios.post('http://localhost:5000/login', loginInfo).then(response=> {
                 console.log('user found through axios')
@@ -30,12 +34,15 @@ class Login extends Component {
             })
             .catch((err)=> {
                 this.setState({isRedirect: false});
+                alert('yuhhhhhh');
                 console.log('user not found through axios');
             })
+            console.log(this.state.isRedirect);
 
     };
     
     render(){
+        const isValid = this.state.email.length > 0 || this.state.password.length > 0;
     return (
         <div className="Login">
             <div className="inputBox">
@@ -45,11 +52,36 @@ class Login extends Component {
                     Email:<br/> 
                     <input type="text" name="email" value={this.state.email} onChange={this.handleEmail.bind(this)} /><br></br>
                     Password: <br/>
-                    <input type="text" name="password" value={this.state.password} onChange={this.handlePassword.bind(this)}/><br></br>
+                    <input type="password" name="password" value={this.state.password} onChange={this.handlePassword.bind(this)}/><br></br>
+                    { isValid? null: <div className='invalid-feedback'>please enter email/password</div> }
+
+                    {/* if(!this.state.isRedirect){
+                        <div> ERROR</div> 
+                    } else {
+                        <div> NOT ERROR </div>
+                    } */}
+
+                        {/* {this.state.isRedirect? (
+                            <div> should redirect </div>
+                        ): (
+                            <div> should not redirect must error</div> 
+                        )} */}
+                        {/* {this.state.receivedRequest && this.state.isRedirect && <div> should not redirect must error</div> }
+                        {this.state.receivedRequest && this.state.isRedirect && <Redirect to={{
+                    pathname: '/timeline'
+                }}/>} */}
+
+                    {/* isLoggedIn ? (
+        <LogoutButton onClick={this.handleLogoutClick} />
+      ) : (
+        <LoginButton onClick={this.handleLoginClick} />
+      )} */}
+
                     <input type="submit" value="Submit"/>
                 </form>
                 <br/>
                 <NavLink to="/createaccount">New User?</NavLink>
+
                 {this.state.isRedirect && <Redirect to={{
                     pathname: '/timeline'
                 }}/>}
