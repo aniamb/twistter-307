7f7c4ff618e4cfb1a0d1b138bd00f48c34d18c1a
Netra Pradhan <npradha@purdue.edu>
2019-10-22 03:23:58 -0400

    Add basic UI to user profile

--- twistter/src/UserProfile.js
+++ twistter/src/UserProfile.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {Switch, Route, NavLink, Redirect} from 'react-router-dom'
-import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 import './UserProfile.css';
 
@@ -9,11 +11,53 @@ class UserProfile extends Component {
         super(props);
     }
  render(){
+    
     return (
         <div className="UserProfile">
-           <h1>USER PROFILE PAGE YA YEET</h1>
-        </div>
+        <br/>
+         <div className="container">
+            <div className="row">
+                {/* User Profile */}
+                <div className="column">
+                    <img id="settings" src={require('/Users/netrapradhan/twistter-307/twistter/src/images/gear.png')}/>
+                        <div className="circle"/>
+                        <br/>
+                        <h3>Netra Pradhan</h3>
+                        <h6>@netrapradhan</h6>
+                        <p>I hate 307 yolo swag</p>
+                        <hr/>
+                        <p>+Followers</p>
+                        <p>+Following</p>
+                        <p>My Topics</p>
+                            <p>
+                                <span id = "topics">CS</span>
+                                <span id = "topics">Math</span>
+                                <span id = "topics">English</span>
+                                <span id = "topics">History</span> 
+                            </p>
+                </div>
+                <div className='double-column'>
+                    <div className='twist'>
+                        Hate CS
+                    </div>
+                    <div className='twist'>
+                        Love CS
+                    </div>
+                    <div className='twist'>
+                        Hate CS
+                    </div>
+                    <div className='twist'>
+                        Love CS
+                    </div>
+                    <div className='twist'>
+                        Hate CS
+                    </div>
+                </div>
+            </div>
         
+        </div> 
+    
+    </div>  
     )
     }
 }
