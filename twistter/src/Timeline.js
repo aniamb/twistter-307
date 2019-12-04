9f5aa00a645563b9120761aaf4605e83078b52e3
Anita <aambalav@purdue.edu>
2019-12-04 00:12:20 -0500

    fixing accessibility

--- twistter/src/Timeline.js
+++ twistter/src/Timeline.js
@@ -195,6 +195,7 @@ class Timeline extends Component{
                 <div className="row-timeline">
                   <div className="sidebar" >
                         <div className="links">
+                          <header>
                             <ul className="navLinks">
                                 <li><NavLink to="/timeline">Twistter</NavLink></li>
                                 <li><NavLink to="/userprofile">My Profile</NavLink></li>
@@ -215,13 +216,16 @@ class Timeline extends Component{
                                     }}/>}
                                 </li>
                             </ul>
+                            </header>
                         </div>
                     </div>
                     <div className="microOrder">
                         <div className="microblogs">
                             <form id="blogID" onSubmit={this.handleBlogPosting.bind(this)}>
-                                Create a new microblog: <br/>
-                                <input type="text" placeholder="Text goes here.." maxLength="280" name="microblog" onChange={this.handlePostBody.bind(this)}></input>
+                                <br/>
+                                <label for="microblog">Create a new microblog: <br></br>
+                                  <input type="text" placeholder="Text goes here.." maxLength="280" name="microblog" onChange={this.handlePostBody.bind(this)}></input>
+                                </label>
                                 <br/>
                                 <input type="submit" value = "Post!"/>
                                 {this.state.errorMessage ? <p> Post must be less than 280 characters: </p> : '' }
